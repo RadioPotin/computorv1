8cf5c4962575e31645988367e0440f95fd13116d
zapashcanon <leo@ndrs.fr>
2021-04-05 17:19:13 +0200

    fix build

--- src/lexer.ml
+++ src/lexer.ml
@@ -28,23 +28,21 @@
 
   *)
 
-module Lexer =
-struct
-open Printf
 open Str
+open Printf
 
-	type terminals =
-		Nbr of string
-	|Var of char
-	|Open_p of char
-	|Clos_p of char
-	|Pow of char
-	|Minus of char
-	|Plus of char
-	|Mult of char
-	|Divi of char
-	|Equa of char
-	|End
+type terminals =
+    Nbr of string
+  |Var of char
+  |Open_p of char
+  |Clos_p of char
+  |Pow of char
+  |Minus of char
+  |Plus of char
+  |Mult of char
+  |Divi of char
+  |Equa of char
+  |End
 
 (*
 	Regex for number identification,
@@ -66,55 +64,53 @@ let re_equ = regexp "="
 let re_spc = regexp "[ \n\r\x0c\t]+"
 
 let rec print_toklist l =
-		match l with
-	| [] -> printf "End of Token List"
-	| x::r -> match x with
-			| Nbr x -> printf "[ %s ]" x; print_toklist r
-			| Var x -> printf "[ %c ]" x; print_toklist r
-			| Open_p x -> printf "[ ( ]"; print_toklist r
-			| Clos_p x -> printf "[ ) ]"; print_toklist r
-			| Pow x -> printf "[ ^ ]"; print_toklist r
-			| Minus x -> printf "[ - ]"; print_toklist r
-			| Plus x -> printf "[ + ]"; print_toklist r
-			| Mult x -> printf "[ * ]"; print_toklist r
-			| Divi x -> printf "[ / ]"; print_toklist r
-			| Equa x -> printf "[ = ]"; print_toklist r
-			| End -> printf "[ END ]\n"; print_toklist r
+  match l with
+  | [] -> printf "End of Token List"
+  | x::r -> match x with
+    | Nbr x -> printf "[ %s ]" x; print_toklist r
+    | Var x -> printf "[ %c ]" x; print_toklist r
+    | Open_p _x -> printf "[ ( ]"; print_toklist r
+    | Clos_p _x -> printf "[ ) ]"; print_toklist r
+    | Pow _x -> printf "[ ^ ]"; print_toklist r
+    | Minus _x -> printf "[ - ]"; print_toklist r
+    | Plus _x -> printf "[ + ]"; print_toklist r
+    | Mult _x -> printf "[ * ]"; print_toklist r
+    | Divi _x -> printf "[ / ]"; print_toklist r
+    | Equa _x -> printf "[ = ]"; print_toklist r
+    | End -> printf "[ END ]\n"; print_toklist r
 
 exception UnknownToken of string
 exception UnauthorizedSyntax of string
 
 let lexer raw =
-	let rec tok pos s =
-		if pos >= String.length s then [End]
-		else if ((Str.string_match re_pnum s pos)
-				|| (Str.string_match re_num s pos)) then
-			let token = Str.matched_string s in
-				(Nbr token)::(tok (pos + String.length token) s);
-		else if (Str.string_match re_sum s pos) then
-			Plus '+'::(tok (pos + 1) s)
-		else if (Str.string_match re_mul s pos) then
-			Mult '*'::(tok (pos + 1) s)
-		else if (Str.string_match re_div s pos) then
-			Divi '/'::(tok (pos + 1) s)
-		else if (Str.string_match re_sub s pos) then
-			Minus '-'::(tok (pos + 1) s)
-		else if (Str.string_match re_pow s pos) then
-			Pow '^'::(tok (pos + 1) s)
-		else if (Str.string_match re_var s pos) then
-			let token = Str.matched_string s in
-				(Var token.[0])::(tok (pos + 1) s)
-		else if (Str.string_match re_ope s pos) then
-			Open_p '('::(tok (pos + 1) s)
-		else if (Str.string_match re_clo s pos) then
-			Clos_p ')'::(tok (pos + 1) s)
-		else if (Str.string_match re_equ s pos) then
-			Equa '='::(tok (pos + 1) s)
-		else if (Str.string_match re_spc s pos) then
-			(tok (pos + 1) s)
-		else
-			raise (UnknownToken "No token was generated")
-	in
-	tok 0 raw
-
-end
+  let rec tok pos s =
+    if pos >= String.length s then [End]
+    else if ((Str.string_match re_pnum s pos)
+             || (Str.string_match re_num s pos)) then
+      let token = Str.matched_string s in
+      (Nbr token)::(tok (pos + String.length token) s);
+    else if (Str.string_match re_sum s pos) then
+      Plus '+'::(tok (pos + 1) s)
+    else if (Str.string_match re_mul s pos) then
+      Mult '*'::(tok (pos + 1) s)
+    else if (Str.string_match re_div s pos) then
+      Divi '/'::(tok (pos + 1) s)
+    else if (Str.string_match re_sub s pos) then
+      Minus '-'::(tok (pos + 1) s)
+    else if (Str.string_match re_pow s pos) then
+      Pow '^'::(tok (pos + 1) s)
+    else if (Str.string_match re_var s pos) then
+      let token = Str.matched_string s in
+      (Var token.[0])::(tok (pos + 1) s)
+    else if (Str.string_match re_ope s pos) then
+      Open_p '('::(tok (pos + 1) s)
+    else if (Str.string_match re_clo s pos) then
+      Clos_p ')'::(tok (pos + 1) s)
+    else if (Str.string_match re_equ s pos) then
+      Equa '='::(tok (pos + 1) s)
+    else if (Str.string_match re_spc s pos) then
+      (tok (pos + 1) s)
+    else
+      raise (UnknownToken "No token was generated")
+  in
+  tok 0 raw
