c071c230a37c567040f9d29997acc990a89d01a1
zapashcanon <leo@ndrs.fr>
2021-04-07 11:57:31 +0200

    add boilerplate for ast

--- src/pp.ml
+++ src/pp.ml
@@ -1,1 +1,15 @@
 (* This file will contain all pretty-printing for computorv1 *)
+
+open Ast
+
+let pp_mono fmt _bla =
+  Format.fprintf fmt "TODO"
+
+let pp_poly fmt _bla =
+  Format.fprintf fmt "TODO"
+
+let pp_eq fmt (left, right) =
+  Format.fprintf fmt "%a = %a@." pp_poly left pp_poly right
+
+let pp_std (eq : equation) =
+  pp_eq Format.std_formatter eq
