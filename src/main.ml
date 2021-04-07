4a14bb66d2d8b16e7b1022a5cabde524d93292b1
RadioPotin <dariopinto@hotmail.fr>
2021-04-07 17:43:35 +0200

    do solver

--- src/main.ml
+++ src/main.ml
@@ -2,7 +2,9 @@ open Computor_lib
 
 let main _argc _argv =
   Pp.std ([], []);
-  Pp.std([(3, Some ("x", 2));(-4, Some("x", 1)); (7, None)], [(28,None)])
+  Solver.solve ([], []);
+  Pp.std([(3, Some ("x", 2));(-4, Some("x", 1)); (7, None)], [(28,None)]);
+  Solver.solve ([(3, Some ("x", 2));(-4, Some("x", 1)); (7, None)], [(28,None)])
 
 let () =
   main (Array.length Sys.argv) Sys.argv
--- src/solver.ml
+++ src/solver.ml
@@ -12,9 +12,9 @@
       if (\delta < 0) then
          solution found with imaginary numbers
       else if (\delta = 0) then
-         one solution possible
+        -b/2a
       else if (\delta > 0) then
-         2 possible solutions.
+       -b-racined/2a et -b+racine/2a
 
   - Solving for x:
 
@@ -22,3 +22,45 @@
     bx = -c
     x = (-c)/b
 *)
+
+let solve e =
+  let left, right = e in
+  let right' = List.map (fun (coeff, var) -> (-coeff, var)) right in
+  let polynome = left @ right' in
+  let seen = ref None in
+  List.iter (fun (_coeff, var)->
+    match var with
+    |None -> ()
+    |Some (var, _n)->
+      begin
+        match !seen with
+        |None -> seen := Some var
+        |Some seen -> if seen <> var then (Format.eprintf "There are two different variable `%s` and `%s`!@." var seen; exit 1)
+      end
+  ) polynome;
+  let a = ref 0 in
+  let b = ref 0 in
+  let c = ref 0 in
+  List.iter (fun (coeff, var)->
+    match var with
+    |None -> c := !c + coeff
+    |Some (_var, n)->
+      if n = 1 then b := !b + coeff
+      else if n = 2 then a := !a + coeff
+      else (Format.eprintf "Power of %d is unsupported@." n; exit 1)
+  ) polynome;
+  let a = !a in
+  let b = !b in
+  let c = !c in
+  Format.printf "a = %d; b = %d; c = %d@." a b c;
+
+  let delta = b * b - 4 * a * c in Format.printf "delta = %d@." delta;
+  let b = float_of_int b in
+  let a = float_of_int a in
+  match delta with
+  |0 -> let sol = ~-.b /. (2. *. a) in Format.printf "x0 = %f@." sol
+  |delta when delta > 0 ->
+    let x1 = (~-.b -. sqrt (float_of_int delta))/. (2. *. a) in
+    let x2 = (~-.b +. sqrt (float_of_int delta))/. (2. *. a) in
+    Format.printf "x1 = %f and x2 = %f@." x1 x2
+  |delta -> assert (delta < 0); ()
