f0b420a50251e70fa312401bb6fcadc7f251f35d
RadioPotin <dariopinto@hotmail.fr>
2021-04-16 17:02:14 +0200

    Make public, prepare testing coverage, change lexer input type

--- src/computor.ml
+++ src/computor.ml
@@ -1,4 +1,4 @@
-open Computor_lib
+open Computor
 
 let usage () =
   Format.eprintf "usage: %s <file>@." Sys.argv.(0);
@@ -6,8 +6,7 @@ let usage () =
 
 let () =
   if Array.length Sys.argv <> 2 then usage () ;
-  let chan = open_in Sys.argv.(1) in
-  let buf = Lexing.from_channel chan in
+  let chan = Sys.argv.(1) in
+  let buf = Lexing.from_string chan in
   let file = Parser.equation Lexer.token buf in
-  close_in chan;
   Solver.solve file
--- test/test.ml
+++ test/test.ml
@@ -1,37 +1,9 @@
-open OUnit2
-
-let test_sanity _ =
-  assert_equal 1 1 ~msg:"Custom error message"
 (*
-
-let lexer_test_simple_int _ =
-  assert_equal [Nbr "5"; End]
-					(lexer "5") ~msg:"Assert Simple Ints 1.";
-  assert_equal [Nbr "5"; Minus '-'; Nbr "8"; End]
-					(lexer "5-8") ~msg:"Assert Simple Ints 2.";
-  assert_equal [Nbr "5"; Divi '/'; Nbr "(-8)"; End]
-					(lexer "5 / (-8)") ~msg:"Assert Simple Ints 3.";
-  assert_equal [Nbr "(+5)"; Mult '*'; Nbr "5"; Pow '^'; Nbr "7"; Plus '+'; Var 'X'; End]
-					(lexer "(+5) * 5^7 + X") ~msg:"Assert Simple Ints 4.";
-  assert_equal [Open_p '('; Nbr "5"; Plus '+'; Nbr "8"; Mult '*'; Nbr "8"; Clos_p ')'; Mult '*'; Nbr "8"; End]
-					(lexer "(5 + 8 * 8) * 8") ~msg:"Assert Simple Ints 5.";
-  assert_equal [End] (lexer "") ~msg:"Assert Simple Ints 6.";
-  assert_equal [End] (lexer "") ~msg:"Assert Simple Ints 7."
-
-let lexer_test_simple_float _ =
-  assert_equal [End] (lexer "") ~msg:"Assert Simple Floats 1."
-
-let lexer_test_simple_equation _ =
-  assert_equal [End] (lexer "") ~msg:"Assert Simple equation fails"
+open Computor
 *)
-let suite =
-  "student" >::: [
-    "sanity" >:: test_sanity
-	(*
-    "simple_int" >:: lexer_test_simple_int;
-    "simple_float" >:: lexer_test_simple_float;
-	"simple_equation">:: lexer_test_simple_equation
-	*)
-  ]
 
-let () = run_test_tt_main suite
+let () = assert (String.equal "" "")
+
+  (*
+  Solver.solve (Parser.equation Lexer.token (Lexing.from_string "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0"));
+   *)
