e269836b183c0b74905fbe0ff1070c743662f499
RadioPotin <dariopinto@hotmail.fr>
2021-04-20 16:25:45 +0200

    Add more tests

--- test/test_computor.ml
+++ test/test_computor.ml
@@ -22,5 +22,17 @@ let () =
   assert (String.equal "Reduced form: 5 * X^0 + 10 * X^1 + 5 * X^2 = 0\nPolynomial degree: 2\nThe solution is:\n-1\n" s);
 
   let s =
+    Format.asprintf "%a" Solver.solve (Parser.equation Lexer.token (Lexing.from_string "5 * X^0 + 13 * X^1 + 3 * X^2 = 1 * X^0 + 1 * X^1")) in
+  assert (String.equal "Reduced form: 4 * X^0 + 12 * X^1 + 3 * X^2 = 0\nPolynomial degree: 2\nDiscriminant is strictly positive, the two solutions are:\n-3.63299\n-0.367007\n" s);
+
+  let s =
+    Format.asprintf "%a" Solver.solve (Parser.equation Lexer.token (Lexing.from_string "5 * X^0 = 4 * X^0 + 7 * X^1" )) in
+  assert (String.equal "Reduced form: 1 * X^0 - 7 * X^1 = 0\nPolynomial degree: 1\nThe solution is:\n0.142857\n" s);
+
+  let s =
+    Format.asprintf "%a" Solver.solve (Parser.equation Lexer.token (Lexing.from_string "4 * X^0 = 8" )) in
+  assert (String.equal "Reduced form: -4 * X^0 = 0\nImpossible.\n" s);
+
+  let s =
     Format.asprintf "%a" Solver.solve (Parser.equation Lexer.token (Lexing.from_string "5 * X^0 + 3 * X^1 + 3 * X^2 = 1 * X^0 + 0 * X^1")) in
   assert (String.equal "Reduced form: 4 * X^0 + 3 * X^1 + 3 * X^2 = 0\nPolynomial degree: 2\nDiscriminant is strictly negative, there is no solution with real numbers:\nComplex solution here.\n" s)
