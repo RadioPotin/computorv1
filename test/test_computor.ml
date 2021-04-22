89c89a37ef40605f48689e6cc692fcf7d2600e8e
RadioPotin <dariopinto@hotmail.fr>
2021-04-22 11:48:44 +0200

    Clarify test_computor.ml

--- test/test_computor.ml
+++ test/test_computor.ml
@@ -3,33 +3,32 @@ open Ast
 
 let eq_to_string s = Format.asprintf "%a" Solver.solve (Parser.equation Lexer.token (Lexing.from_string s))
 
-
+let handle_test number expected given =
+  Format.printf "Test %d: %s@." number given;
+  assert (String.equal expected (eq_to_string given))
 
 let () =
   (*
    * TEST 0
    *)
-  let s =
-    Format.printf "Test 0: 5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0\n";
-    eq_to_string "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0" in
-  assert (String.equal "Reduced form: 4 * X^0 + 4 * X^1 - 9.3 * X^2 = 0\nPolynomial degree: 2\nDiscriminant is strictly positive, the two solutions are:\n0.905239\n-0.475131\n" s);
+  handle_test 0
+    "Reduced form: 4 * X^0 + 4 * X^1 - 9.3 * X^2 = 0\nPolynomial degree: 2\nDiscriminant is strictly positive, the two solutions are:\n0.905239\n-0.475131\n"
+    "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0";
 
   (*
    * TEST 1
    *)
-  let s =
-    Format.printf "Test 1: 5 * X^0 + 4 * X^1 = 4 * X^0\n";
-    eq_to_string "5 * X^0 + 4 * X^1 = 4 * X^0" in
-  assert (String.equal "Reduced form: 1 * X^0 + 4 * X^1 = 0\nPolynomial degree: 1\nThe solution is:\n-0.25\n" s);
+  handle_test 1
+    "Reduced form: 1 * X^0 + 4 * X^1 = 0\nPolynomial degree: 1\nThe solution is:\n-0.25\n"
+    "5 * X^0 + 4 * X^1 = 4 * X^0";
 
   (*
    * TEST 2
    *)
   try
-    let s =
-      Format.printf "Test 2: 8 * X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 = 3 * X^0\n";
-      eq_to_string "8 * X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 = 3 * X^0" in
-    assert (String.equal "Reduced form: 5 * X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 = 0\nPolynomial degree: 3\nThe polynomial degree is stricly greater than 2, I can't solve.\n" s);
+    handle_test 2
+      "Reduced form: 5 * X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 = 0\nPolynomial degree: 3\nThe polynomial degree is stricly greater than 2, I can't solve.\n"
+      "8 * X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 = 3 * X^0";
   with
   | Big_degree -> ();
 
@@ -37,55 +36,46 @@ let () =
   (*
    * TEST 3
    *)
-    let s =
-      Format.printf "Test 3: 5 + 4 * X + X^2 = X^2\n";
-      eq_to_string "5 + 4 * X + X^2 = X^2" in
-    assert (String.equal "Reduced form: 5 * X^0 + 4 * X^1 = 0\nPolynomial degree: 1\nThe solution is:\n-1.25\n" s);
-
+    handle_test 3
+      "Reduced form: 5 * X^0 + 4 * X^1 = 0\nPolynomial degree: 1\nThe solution is:\n-1.25\n"
+      "5 + 4 * X + X^2 = X^2";
   (*
    * TEST 4
    *)
-    let s =
-      Format.printf "Test 4: 6 * X^0 + 11 * X^1 + 5 * X^2 = 1 * X^0 + 1 * X^1\n";
-      eq_to_string "6 * X^0 + 11 * X^1 + 5 * X^2 = 1 * X^0 + 1 * X^1" in
-    assert (String.equal "Reduced form: 5 * X^0 + 10 * X^1 + 5 * X^2 = 0\nPolynomial degree: 2\nThe solution is:\n-1\n" s);
+    handle_test 4
+      "Reduced form: 5 * X^0 + 10 * X^1 + 5 * X^2 = 0\nPolynomial degree: 2\nThe solution is:\n-1\n"
+      "6 * X^0 + 11 * X^1 + 5 * X^2 = 1 * X^0 + 1 * X^1";
 
   (*
    * TEST 5
    *)
-    let s =
-      Format.printf "Test 5: 5 * X^0 + 13 * X^1 + 3 * X^2 = 1 * X^0 + 1 * X^1\n";
-      eq_to_string "5 * X^0 + 13 * X^1 + 3 * X^2 = 1 * X^0 + 1 * X^1" in
-    assert (String.equal "Reduced form: 4 * X^0 + 12 * X^1 + 3 * X^2 = 0\nPolynomial degree: 2\nDiscriminant is strictly positive, the two solutions are:\n-3.63299\n-0.367007\n" s);
+    handle_test 5
+      "Reduced form: 4 * X^0 + 12 * X^1 + 3 * X^2 = 0\nPolynomial degree: 2\nDiscriminant is strictly positive, the two solutions are:\n-3.63299\n-0.367007\n"
+      "5 * X^0 + 13 * X^1 + 3 * X^2 = 1 * X^0 + 1 * X^1";
 
   (*
    * TEST 6
    *)
-    let s =
-      Format.printf "Test 6: 5 * X^0 = 4 * X^0 + 7 * X^1\n";
-      eq_to_string "5 * X^0 = 4 * X^0 + 7 * X^1" in
-    assert (String.equal "Reduced form: 1 * X^0 - 7 * X^1 = 0\nPolynomial degree: 1\nThe solution is:\n0.142857\n" s);
+    handle_test 6
+      "Reduced form: 1 * X^0 - 7 * X^1 = 0\nPolynomial degree: 1\nThe solution is:\n0.142857\n"
+      "5 * X^0 = 4 * X^0 + 7 * X^1";
 
   (*
    * TEST 7
    *)
-    let s =
-      Format.printf "Test 7: 4 * X^0 = 8 * X^0\n";
-      eq_to_string "4 * X^0 = 8 * X^0" in
-    assert (String.equal "Reduced form: -4 * X^0 = 0\nImpossible.\n" s);
+    handle_test 7
+      "Reduced form: -4 * X^0 = 0\nImpossible.\n"
+      "4 * X^0 = 8 * X^0";
 
   (*
    * TEST 8
    *)
-    let s =
-      Format.printf "Test 8: 5 * X^0 + 3 * X^1 + 3 * X^2 = 1 * X^0 + 0 * X^1\n";
-      eq_to_string "5 * X^0 + 3 * X^1 + 3 * X^2 = 1 * X^0 + 0 * X^1" in
-    assert (String.equal "Reduced form: 4 * X^0 + 3 * X^1 + 3 * X^2 = 0\nPolynomial degree: 2\nDiscriminant is strictly negative, there is no solution with real numbers:\nComplex solution here.\n" s);
-
+    handle_test 8
+      "Reduced form: 4 * X^0 + 3 * X^1 + 3 * X^2 = 0\nPolynomial degree: 2\nDiscriminant is strictly negative, there is no solution with real numbers:\nComplex solution here.\n"
+      "5 * X^0 + 3 * X^1 + 3 * X^2 = 1 * X^0 + 0 * X^1";
   (*
    * TEST 9
    *)
-    let s =
-      Format.printf "Test 9: 5 * X^0 = 5 * X^0\n";
-      eq_to_string "5 * X^0 = 5 * X^0" in
-    assert (String.equal "Reduced form: 0 = 0\nPolynomial degree: 0\nThe solution is:\nAll real numbers are solutions.\n" s);
+    handle_test 9
+      "Reduced form: 0 = 0\nPolynomial degree: 0\nThe solution is:\nAll real numbers are solutions.\n"
+      "5 * X^0 = 5 * X^0";
