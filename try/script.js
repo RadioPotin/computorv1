b61c299e3aa7760ead7ae98d3d51001f169605b4
RadioPotin <laradiopotin@gmail.com>
2021-05-26 11:22:04 +0200

    add web interface for compv

--- try/script.js
+++ try/script.js
@@ -2,25 +2,25 @@
 // @flow
 "use strict";
 
-window.onload = main;
-function fun()
-{
-  const text = document.getElementById("textbox");
-  if (!text) {
-    throw "invalid textbox";
-  }
-  const result = document.getElementById("output");
-  if (!result) {
-    throw "invalid result";
-  }
-  result.innerHTML = text.itemValue;
+const button = document.getElementById("butn");
+if (!button) {
+  throw "invalid button";
 }
-function main()
-{
-  const button = document.getElementById("butn");
-  if (!button) {
-    throw "invalid button";
-  }
-  button.addEventListener("click", fun);
-  return;
+
+const text = document.getElementById("textbox");
+if (!text) {
+  throw "invalid textbox";
 }
+if (!(text instanceof HTMLInputElement))
+{
+  throw "textbox not HTMLInputElement";
+}
+
+const result = document.getElementById("output");
+if (!result) {
+  throw "invalid result";
+}
+
+button.addEventListener("click", function () {
+  result.innerHTML = text.value;
+} );
