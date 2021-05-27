13c1d27a31c7a33cb5795703913d551d1e0ec141
RadioPotin <31403378+RadioPotin@users.noreply.github.com>
2021-05-27 13:10:53 +0000

    Deploying to gh-pages from  @ 9a5182c83adf0f57248501505aa6724f8ae4656a 🚀

--- try/trycomputorv1.ml
+++ try/trycomputorv1.ml
@@ -1,6 +1,5 @@
 open Js_of_ocaml
 open Computor
-open Ast
 
 let error s =
   Format.eprintf "%s@." s;
@@ -23,37 +22,13 @@ let button =
   Js.coerce_opt button Dom_html.CoerceTo.button (fun _ ->
       error "Invalid button" )
 
-let result s =
-  try
-    let ast =
-      Parser.equation Lexer.token (Lexing.from_string (Js.to_string s))
-    in
-    let polynomes = Solver.ast_to_lists ast in
-    Format.asprintf "%a" Solver.solve polynomes
-  with
-  | Big_degree ->
-    "Highest degree is superior to 2, please give a second degree equation."
-  | Too_many_variables ->
-    "There are more than one variable in this equation, please give only one."
-  | Syntax_error _ -> "There is a syntax error in this input."
-  | Parser.Error -> "There is an unknown token in this input."
-
-let convert_newlines s =
-  let buff = Buffer.create 512 in
-  let fmt = Format.formatter_of_buffer buff in
-  String.iter
-    (function
-      | '\n' -> Format.fprintf fmt "\n<br />"
-      | c -> Format.fprintf fmt "%c" c )
-    s;
-  Buffer.contents buff
-
 let () =
   let _id =
     Dom_html.addEventListener button Dom_html.Event.click
       (Dom_html.handler (fun _ ->
            blockquote##.innerHTML :=
-             Js.string (convert_newlines (result textarea##.value));
+             Js.string @@ Try_utils.convert_newlines
+             @@ Try_utils.result (Js.to_string textarea##.value);
            Js.bool true ) )
       (Js.bool false)
   in
