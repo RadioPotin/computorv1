ba99b8bb8481fb3f45f3e2fec0e386fcadd072aa
RadioPotin <laradiopotin@gmail.com>
2021-05-28 12:08:27 +0200

    regroup try functions

--- try/trycomputorv1.ml
+++ try/trycomputorv1.ml
@@ -1,5 +1,33 @@
 open Js_of_ocaml
 open Computor
+open Ast
+
+let result s =
+  try
+    let ast = Parser.equation Lexer.token (Lexing.from_string s) in
+    let polynomes = Solver.ast_to_lists ast in
+    Format.asprintf "%a" Solver.solve polynomes
+  with
+  | Big_degree ->
+    "Highest degree is superior to 2, please give a second degree equation."
+  | Too_many_variables ->
+    "There are more than one variable in this equation, please give only one."
+  | Syntax_error _ -> "There is an unknown token in this input."
+  | Parser.Error -> "There is a syntax error in this input."
+  | e ->
+    Format.eprintf "ERROR: %s" (Printexc.to_string e);
+    raise e
+
+let convert_newlines s =
+  let buff = Buffer.create 512 in
+  let fmt = Format.formatter_of_buffer buff in
+  String.iter
+    (function
+      | '\n' -> Format.fprintf fmt "\n<br />"
+      | c -> Format.fprintf fmt "%c" c )
+    s;
+  Format.pp_print_flush fmt ();
+  Buffer.contents buff
 
 let error s =
   Format.eprintf "%s@." s;
@@ -27,8 +55,8 @@ let () =
     Dom_html.addEventListener button Dom_html.Event.click
       (Dom_html.handler (fun _ ->
            blockquote##.innerHTML :=
-             Js.string @@ Try_utils.convert_newlines
-             @@ Try_utils.result (Js.to_string textarea##.value);
+             Js.string @@ convert_newlines
+             @@ result (Js.to_string textarea##.value);
            Js.bool true ) )
       (Js.bool false)
   in
